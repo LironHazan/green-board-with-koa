0457fec4baeaf7236ea43d6f6435107f0298cd1e
LironHazan <liron@gigaspaces.com>
2015-10-24 19:12:02 +0300

    using ui-router

--- app/GreenBoard/GitBoard/Login/LoginCtrl.js
+++ app/GreenBoard/GitBoard/Login/LoginCtrl.js
@@ -1,20 +1,22 @@
 /**
  * Created by liron on 10/17/15.
  */
-angular.module('GreenBoard').controller('GitLoginCtrl', function ( $scope, item, $modalInstance, $log, GitLoginService) {
+angular.module('GreenBoard').controller('GitLoginCtrl', function ( $scope, $rootScope, item, $modalInstance, $log, GitLoginService) {
 //modal
 
     $scope.item = item;
-   // $scope.isLogin = false;
+    $scope.isLogin = false;
 
     $scope.login = function () {
 
         if ($scope.item && $scope.item.email && $scope.item.passwd) {
            // $scope.isLogin = true;
-            GitLoginService.login({email:$scope.item.email, passwd:$scope.item.passwd}).then( function success (){
+            GitLoginService.login({email:$scope.item.email, passwd:$scope.item.passwd}).then( function success (data){
+                $modalInstance.dismiss('login');
+                $rootScope.$emit('userLoggedIn', data);
 
             }, function error(err){
-              //  $scope.isLogin = false;
+                $scope.isLogin = false;
 
             });
 
